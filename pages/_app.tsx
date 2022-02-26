b75b11dc140e7df6f0739db659008ac32757574a
dash7ou <fxfddfno@gmail.com>
2022-02-26 12:16:08 -0500

    start witt link and global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from 'next/app';
+import  Link from "next/link"
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6" style={{backgroundColor: "purple"}}>
+        <p className="text4x1 font-bold text-white">Crepto Birdz MarketPlace</p>
+        <div className="flex mt-4 justify-center">
+          <Link href="/">
+            <a className="mr-4">Main Marketplace</a>
+          </Link>
+          <Link href="/mint-nfts">
+            <a className="mr-6">Mint Token</a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6">My NFTs</a>
+          </Link>
+          <Link href="/account-dashboard">
+            <a className="mr-4">Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
-export default MyApp
+export default MyApp;
