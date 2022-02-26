30b9863bf2394987e4251cb63b0ce2944b6fc88a
dash7ou <fxfddfno@gmail.com>
2022-02-26 12:43:02 -0500

    Deploy smartcontract

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 import type { NextPage } from 'next';
+import {ethers} from "ethers";
+import { useEffect, useState } from 'react';
+import axios from "axios"
+import Web3 from "web3modal";
 
 const Home: NextPage = () => {
   return (
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const NFTMarket = await hre.ethers.getContractFactory("KBMarket");
+  const nftMarket = await NFTMarket.deploy();
+  await nftMarket.deployed();
+
+  const NFT = await hre.ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy(nftMarket.address);
+  await nft.deployed();
+
+
+  let config = `export const nftMarketAddress = ${nftMarket.address};
+export const nftAddress = ${nft.address};`;
+
+  const data = JSON.stringify(config);
+  fs.writeFileSync("config.js", JSON.parse(data));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
